8416dc54e20d3bfaa70ed35279f1e5a2cf4a7308
kalle hålldén <kallehalldeen@gmail.com>
2018-12-15 10:13:29 +0100

    Menu added for top navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
