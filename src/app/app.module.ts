b6cb9d66c5919d1bfca52a5c2ed9d96dca4b38b0
kalle hålldén <kallehalldeen@gmail.com>
2018-12-20 19:51:51 +0100

    Added new components and started the Projects page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', component: HomeComponent }
 ]
 
 @NgModule({
@@ -17,6 +24,9 @@ const routes: Routes = [
     AppComponent,
     ProjectsComponent,
     HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
